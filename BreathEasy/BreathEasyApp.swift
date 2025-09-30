6cea5ba6f1d28a31416dc4b1c74dd31489c45664
Rajkumar Natarajan <raj.sn84@outlook.com>
2025-09-30 00:13:26 -0400

    Initial Commit

---
+++ BreathEasy/BreathEasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathEasyApp.swift
+//  BreathEasy
+//
+//  Created by Rajkumar Natarajan on 2025-09-30.
+//
+
+import SwiftUI
+
+@main
+struct BreathEasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
